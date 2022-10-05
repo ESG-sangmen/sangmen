c2dc2dad3c502c4571d1a581b4ff41c5a7d8530e
yunhan8306 <dbsgks17@naver.com>
2022-10-05 20:22:16 +0900

    22.10.05(8)

--- src/main/java/com/sangmen/controller/JoinCon.java
+++ src/main/java/com/sangmen/controller/JoinCon.java
@@ -7,23 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class JoinCon
- */
 @WebServlet("/JoinCon")
 public class JoinCon extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Default constructor.
-	 */
-	public JoinCon() {
-		// TODO Auto-generated constructor stub
-	}
-
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 	}
 
 }
